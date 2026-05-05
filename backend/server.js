5d0b9a8d13d5f8f1a2b7cafd7014044e2047269f
M Rohit <mrohit582003@gmail.com>
2026-05-05 15:28:20 +0530

    Image Upload Updated

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,8 @@ import dashboardRoutes from './routes/dashboardRoutes.js';
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '200mb' }));
+app.use(express.urlencoded({ limit: '200mb', extended: true }));
 app.use(cookieParser());
 
 //Routes
